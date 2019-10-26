eb79f7847003fbd34932775df82e44c5c807abb5
Roger Howell <howelrtc@aston.ac.uk>
2019-10-26 18:08:23 +0100

    initial commit of java web app code

---
+++ src/main/java/com/github/budgettime/web/servlets/MainServlet.java
@@ -0,0 +1,58 @@
+package com.github.budgettime.web.servlets;
+
+//import javax.servlet.annotation.WebServlet;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//@WebServlet(name = "MainServlet", urlPatterns={"/main/*"})
+public class MainServlet extends HttpServlet {
+
+    private final static String CONTENT_TYPE_HTML = "text/html;charset=UTF-8";
+
+    @Override
+    protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("doGet");
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("doPost");
+        processRequest(request, response);
+    }
+
+    @Override
+    public void init() {
+        System.out.println("init");
+        System.out.println("TESTING");
+    }
+
+
+    private void processRequest(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("processRequest");
+        response.setContentType(CONTENT_TYPE_HTML);
+
+        RequestDispatcher dispatcher = null;
+
+        final String      action = request.getPathInfo();
+        final PrintWriter writer = response.getWriter();
+
+        System.out.println("action = " + action);
+
+        if (action.equals("/index")) {
+            dispatcher = this.getServletContext().getRequestDispatcher("/index.jsp");
+        } else {
+            response.setContentType(CONTENT_TYPE_HTML);
+            response.sendError(HttpServletResponse.SC_BAD_GATEWAY);
+        }
+
+        if (dispatcher != null) {
+            dispatcher.forward(request, response);
+        }
+    }
+}
