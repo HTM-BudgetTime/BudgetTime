5b8d1c4d5277574e698c1b50e8ca210476d4802b
Roger Howell <howelrtc@aston.ac.uk>
2019-10-27 03:54:31 +0000

    additional demo test case to show how to run sql queries;

--- src/test/java/DatabaseConnectionTest.java
+++ src/test/java/DatabaseConnectionTest.java
@@ -3,22 +3,38 @@ import org.junit.Test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DatabaseConnectionTest {
 
     @Test
-    public void doTest() throws SQLException {
+    public void testConnectToDatabaseWithoutException() throws SQLException {
         final Database   database   = new Database();
         final Connection connection = database.getConnection();
     }
 
     @Test
-    public void doTest3() throws SQLException {
+    public void testConnectToDatabaseWithoutException2() throws SQLException {
         String dbUri      = System.getenv("BT_DB_URI");
         String dbUsername = System.getenv("BT_DB_USERNAME");
         String dbPassword = System.getenv("BT_DB_PASSWORD");
 
         Connection con = DriverManager.getConnection(dbUri, dbUsername, dbPassword);
+    }
+
+    @Test
+    public void testRunQueryGetData() throws SQLException {
+        final Database   database   = new Database();
+        final Connection connection = database.getConnection();
+
+        final PreparedStatement preparedStatement = connection.prepareStatement("SELECT username, password FROM accounts");
+        final ResultSet         rs                = preparedStatement.executeQuery();
+        while (rs.next()) {
+            String name = rs.getString("username");
+            String pw   = rs.getString("password");
+            System.out.println(name + "   " + pw);
+        }
     }
 }
