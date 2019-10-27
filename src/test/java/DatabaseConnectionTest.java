4350372b96b9cf119be847396c3ecf2661d63068
Roger Howell <howelrtc@aston.ac.uk>
2019-10-27 03:23:11 +0000

    added sql file which will later be used to pre-populate the database with test data;

--- src/test/java/DatabaseConnectionTest.java
+++ src/test/java/DatabaseConnectionTest.java
@@ -15,7 +15,7 @@ public class DatabaseConnectionTest {
 
     @Test
     public void doTest3() throws SQLException {
-        String dbUri      = "jdbc:mysql://db-mysql-lon1-58063-do-user-2199254-0.db.ondigitalocean.com:25060/defaultdb?ssl-mode=REQUIRED";
+        String dbUri      = System.getenv("BT_DB_URI");
         String dbUsername = System.getenv("BT_DB_USERNAME");
         String dbPassword = System.getenv("BT_DB_PASSWORD");
 
